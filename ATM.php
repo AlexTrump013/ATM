ef91b836317fb6eac6a6e5f1e6f0e1f3d4d36ff7
AlexTrump013 <80057678+AlexTrump013@users.noreply.github.com>
2021-10-20 11:19:03 +0300

    Add new functional

--- ATM.php
+++ ATM.php
@@ -13,19 +13,6 @@ class ATM
         rsort($this->bills);
     }
 
-    public function authorization(): bool{
-        fwrite(STDOUT, "Enter your password: \n");
-        $password = trim(fgets(STDIN));
-
-        if ($password === $this->card->password) {
-            echo "Пароль правильный! \n ";
-            return true;
-        } else {
-            echo "Пароль неправильный. Повторите ввод \n ";
-            return false;
-        }
-    }
-
     private function enterCard(){
         $data = NULL;
         while ($data != "Y") {
@@ -33,23 +20,6 @@ class ATM
             $data = trim(fgets(STDIN));
         }
         $this->card = new Card("0000", 10000);
-    }
-
-    public function menu(){
-        fwrite(STDOUT, "Выбирите действие: \n 1 Снять наличные\n 2 Проверить баланс карты\n 0 Выход\n");
-        $actionMenu = trim(fgets(STDIN));
-
-        switch ($actionMenu) {
-            case 1:
-                print ($this->getMoney());
-                $this->menu();
-            case 2:
-                print("Баланс карты:\n $ ".$this->card->money."\n");
-                $this->menu();
-            case 0:
-                echo "Выход\n";
-                $this->start();
-        }
     }
 
     public function start(){
@@ -68,18 +38,95 @@ class ATM
         $this->menu();
     }
 
+    public function authorization(): bool{
+        fwrite(STDOUT, "Введите пароль: \n");
+        $password = trim(fgets(STDIN));
+
+        if ($password === $this->card->password) {
+            return true;
+        } else {
+            echo " ОШИБКА: пароль неправильный. Повторите ввод \n";
+            return false;
+        }
+    }
+
+    public function menu(){
+        fwrite(STDOUT, "Выбирите действие: \n 1. Снять наличные\n 2. Проверить баланс карты\n 3. Пополнить мобильный счет\n 0. Выход\n");
+        $actionMenu = trim(fgets(STDIN));
+        if($actionMenu > 3){
+            echo " ОШИБКА: отсутсвует данный пункт меню\n";
+            $this->menu();
+        }
+        switch ($actionMenu) {
+            case 1:
+                print ($this->getMoney());
+                $this->menu();
+            case 2:
+                print("Баланс карты:\n $ ".$this->card->money."\n");
+                $this->menu();
+            case 3:
+                print($this->topUpPhoneAccount());
+                $this->menu();
+            case 0:
+                echo "Выход\n";
+                $this->start();
+        }
+    }
+
     public function getMoney(){
-        fwrite(STDOUT, "Введите сумму:\n");
+        fwrite(STDOUT, "Выбирите сумму:\n 1. $50\n 2. $100\n 3. $200\n 4. $500\n 5. $1000\n 0. Другая сумма\n");
+        $actionMenu = trim(fgets(STDIN));
+
+        switch ($actionMenu) {
+            case 1:
+                $this->getBills(50);
+                $this->moneyInAtm-=50;
+                $this->card->money-=50;
+                $this->menu();
+            case 2:
+                $this->getBills(100);
+                $this->moneyInAtm-=100;
+                $this->card->money-=100;
+                $this->menu();
+            case 3:
+                $this->getBills(200);
+                $this->moneyInAtm-=200;
+                $this->card->money-=200;
+                $this->menu();
+            case 4:
+                $this->getBills(500);
+                $this->moneyInAtm-=500;
+                $this->card->money-=500;
+                $this->menu();
+            case 5:
+                $this->getBills(1000);
+                $this->moneyInAtm-=1000;
+                $this->card->money-=1000;
+                $this->menu();
+            case 0:
+                $this->anotherAmountOfMoney();
+                $this->menu();
+        }
+    }
+
+    private function anotherAmountOfMoney(){
+        fwrite(STDOUT, "Введите сумму кратную 50:\n");
         $output = trim(fgets(STDIN));
         if($output > $this->moneyInAtm){
-            print("АТМ не может выдать эту сумму. Доступное количество: ".$this->moneyInAtm."\n");
-
+            print(" ОШИБКА: АТМ не может выдать эту сумму. Доступное количество: ".$this->moneyInAtm."\n");
         } else if( $output > $this->card->money){
-            print ("Недостаточно денег на счету!\n");
-        } else {
-            print($this->getBills($output));
-            $this->moneyInAtm-=$output;
-            $this->card->money-=$output;
+            print (" ОШИБКА: недостаточно денег на счету!\n");
+        }
+        else {
+            if($output %50 == 0){
+                $this->getBills($output);
+                $this->moneyInAtm-=$output;
+                $this->card->money-=$output;
+            }
+            else{
+                echo "ОШИБКА: не верно введена сумма!\n";
+                $this->getMoney();
+            }
         }
     }
 
@@ -90,7 +137,7 @@ class ATM
         $this->moneyInAtm -= $withdrawAmount;
         while ($this->money_left > 0) {
             if ($this->money_left < min($this->bills)) {
-                throw new WithdrawException("This amount cannot be paid. \n");
+                throw new WithdrawException("Эта сумма не подлежит оплате. \n");
             }
             $bill = $this->configureBills();
             $this->cash[] = $bill;
@@ -99,7 +146,7 @@ class ATM
         echo "Заберите деньги: \n";
         $money = array_count_values($this->cash);
         foreach ($money as $key => $value){
-            echo "Колличество купюр по $: $key равно $value \n";
+            echo " Колличество купюр по $$key равно $value \n";
         }
     }
 
@@ -114,7 +161,36 @@ class ATM
         }
         return min($this->bills);
     }
+
+    private function topUpPhoneAccount(){
+        fwrite(STDOUT, "Введите номер телефона: +38");
+        $numberPhone = trim(fgets(STDIN));
+        if(!preg_match("/[0-9]{10}/i", $numberPhone)){
+            echo " ОШИБКА: введенный мобильный телефон не соответствует шаблону, пример: +380960969966 \n";
+            $this->topUpPhoneAccount();
+        }
+
+        fwrite(STDOUT, " Введите сумму пополнения счета: ");
+        $moneyTransfer = trim(fgets(STDIN));
+        if($moneyTransfer > $this->card->money){
+            echo " ОШИБКА: введенная сумма больше суммы на карте!\n";
+            fwrite(STDOUT, "Выбирите действие:\n 1. Повторить пополнение счета\n 0. Вернуться в главное меню\n");
+            $actionMenu = trim(fgets(STDIN));
+            switch ($actionMenu) {
+                case 1:
+                    $this->topUpPhoneAccount();
+                case 0:
+                    $this->menu();
+            }
+        }
+        else{
+            echo " Номер телефона +380$numberPhone пополнен на $$moneyTransfer. Хорошего дня!\n";
+            $this->card->money-=$moneyTransfer;
+            $this->menu();
+        }
+    }
 }
 
 $atm = new ATM();
 $atm->start();
+//$atm->menu();
